34a981ae5f89a236bb7e671c40c193aa59210620
VicenteVicenteVicenteVicenteVicente <vecalisto@uc.cl>
2020-08-16 16:09:03 -0400

    fix: Layout Fixes

--- rematch/src/App.js
+++ rematch/src/App.js
@@ -1,46 +1,29 @@
-/**
- * TODO:
- * pagination
- * fixed editor and resizable layout
- * color scheme
- * hover
- * background color differed
- * bug pagination
- * cortar match con (...)
- x botón con más informacion (span, linea, char)
- * alertas material
- * Early outputs
- * download results
- */
 import React, { Component } from 'react';
 
-import Logo from './assets/logo-dark.png';
-
-import CodeMirror from 'codemirror';
-import 'codemirror/theme/material-darker.css';
-import 'codemirror/addon/mode/simple';
-
+/* MaterialUI */
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Button from '@material-ui/core/Button';
-import { PlayArrow, Publish, GetApp } from '@material-ui/icons';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Backdrop from '@material-ui/core/Backdrop';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
 import Tooltip from '@material-ui/core/Tooltip';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-import Pagination from '@material-ui/lab/Pagination';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+
+import { PlayArrow, Publish } from '@material-ui/icons';
+
+/* Project Components */
+import SectionTitle from './components/SectionTitle';
+import ResultsTable from './components/ResultsTable';
+
+/* CodeMirror */
+import CodeMirror from 'codemirror';
+import 'codemirror/theme/material-darker.css';
+import 'codemirror/addon/mode/simple';
+
+/* Assets */
+import Logo from './assets/logo-dark.png';
 
 const WORKPATH = `${process.env.PUBLIC_URL}/work.js`;
 const CHUNKSIZE = 1 * 10 ** 8; // 100MB
@@ -82,147 +65,6 @@ const darkTheme = createMuiTheme({
   },
 });
 
-const SectionTitle = ({ title }) => (
-  <Typography variant="body1" component="div" style={{
-    color: '#000000',
-    backgroundColor: '#03DAC6',
-    padding: '.5rem 0 .5rem .75rem',
-  }}>
-    <Box fontWeight="fontWeightBold">
-      {title}
-    </Box>
-  </Typography>
-)
-
-class ResultsTable extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      page: 0,
-      rowsPerPage: 25,
-    }
-  }
-
-  handleDownload = () => {
-    let blob = new Blob([
-      JSON.stringify({
-        variables: this.props.schema,
-        matches: this.props.spanList
-      })], { type: 'application/json' });
-    let elem = window.document.createElement('a');
-    elem.href = window.URL.createObjectURL(blob);
-    elem.download = 'test.json';
-    document.body.appendChild(elem);
-    elem.click();
-    document.body.removeChild(elem);
-  }
-
-  handleChangePage = (_, newPage) => {
-    this.setState({ page: newPage - 1 });
-  }
-
-  handleChangeRowsPerPage = (event) => {
-    this.setState({
-      rowsPerPage: event.target.value,
-      page: 0,
-    });
-  }
-
-  handleMarkText = (row) => {
-    this.props.clearMarks();
-    this.props.addMarks(row);
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.spanList.length !== 0 && this.props.spanList.length === 0) {
-      this.setState({
-        page: 0,
-      })
-    }
-  }
-
-  render() {
-    const { schema, spanList, textEditor } = this.props;
-    return ([
-      <Grid container spacing={0}>
-        <Grid item xs={7} style={{ margin: '.25rem 0' }}>
-          <Pagination
-            page={this.state.page + 1}
-            style={{ display: 'block' }}
-            count={Math.ceil(spanList.length / this.state.rowsPerPage)}
-            onChange={this.handleChangePage}
-            showFirstButton
-            showLastButton />
-        </Grid>
-        <Grid item xs={5} style={{ margin: '.25rem 0', display: 'flex', justifyContent: 'flex-end', gap: '.5rem' }}>
-          <Select
-            value={this.state.rowsPerPage}
-            labelId="demo-simple-select-label"
-            onChange={this.handleChangeRowsPerPage}>
-            <MenuItem value={10}>10 rows per page</MenuItem>
-            <MenuItem value={25}>25 rows per page</MenuItem>
-            <MenuItem value={50}>50 rows per page</MenuItem>
-            <MenuItem value={100}>100 rows per page</MenuItem>
-          </Select>
-          <Tooltip title="Download matches">
-            <Button color="primary" startIcon={<GetApp />} onClick={this.handleDownload}>
-              Matches
-            </Button>
-          </Tooltip>
-        </Grid>
-      </Grid>,
-      <TableContainer style={{ height: '40vh', oveflow: 'auto' }}>
-        <Table stickyHeader size="small">
-          <colgroup>
-            {schema.map((_, schIdx) => (
-              <col key={schIdx} style={{ width: `${100 * 1 / schema.length}%` }} />
-            ))}
-          </colgroup>
-          <TableHead>
-            {(spanList.length > 0)
-              ? <TableRow>
-                {schema.map((name, idxHead) => (
-                  <TableCell className={`th${idxHead}`} key={idxHead}>
-                    {name}
-                  </TableCell>
-                ))}
-              </TableRow>
-              :
-              <TableRow>
-                <TableCell style={{backgroundColor: 'transparent'}}>
-                  No matches
-                </TableCell>
-              </TableRow>
-            }
-          </TableHead>
-          <TableBody>
-            {(this.state.rowsPerPage > 0
-              ? spanList.slice(
-                this.state.page * this.state.rowsPerPage,
-                this.state.page * this.state.rowsPerPage + this.state.rowsPerPage)
-              : spanList).map((row, idxRow) => (
-                <TableRow key={idxRow} hover style={{ cursor: 'pointer' }} onClick={() => this.handleMarkText(row)}>
-                  {row.map((col, idxCol) => {
-                    let match = textEditor.getRange(textEditor.posFromIndex(col.s), textEditor.posFromIndex(col.e));
-                    return (
-                      <TableCell key={idxCol} style={{ padding: 0 }}>
-                        <div style={{ padding: '6px 24px 6px 16px' }}>
-                          {(match.length > 36)
-                            ? match.slice(0, 36) + '...'
-                            : match}
-                        </div>
-                      </TableCell>)
-                  }
-                  )}
-                </TableRow>
-              ))}
-          </TableBody>
-        </Table>
-      </TableContainer >]
-    )
-  }
-}
-
 /* APP */
 class App extends Component {
   constructor(props) {
@@ -236,7 +78,7 @@ class App extends Component {
   }
   componentDidMount() {
     let queryEditor = CodeMirror(document.getElementById('queryEditor'), {
-      value: '!a{RE}!b{[a-z]+} !c{[a-z]+}!d{!}',
+      value: '!a{[A-Z][a-z]+} !b{is} !c{..} !d{ex[a-z]+} !e{.+}!f{!}',
       mode: 'rematchQuery',
       theme: 'material-darker',
       lineNumbers: false,
@@ -258,12 +100,7 @@ class App extends Component {
       return true;
     });
     let textEditor = CodeMirror(document.getElementById('textEditor'), {
-      value:
-        `REmatch cool!
-REmatch awesome!
-REmatch nice!
-REmatch best!
-`,
+      value: 'This is an example text!',
       mode: 'text/plain',
       theme: 'material-darker',
       lineNumbers: true,
@@ -293,11 +130,13 @@ REmatch best!
     });
     this.state.textEditor.scrollIntoView(start, 200);
   }
+
   clearMarks = () => {
     this.state.textEditor.getAllMarks().forEach((mark) => {
       mark.clear();
     });
   }
+
   handleFile = async (event) => {
     let file = event.target.files[0];
     if (!file) { return };
@@ -319,6 +158,7 @@ REmatch best!
     console.log('upload done');
     this.setState({ uploadingFile: false })
   }
+
   runWorker = () => {
     console.log('STARTED');
     this.clearMarks();
@@ -357,29 +197,47 @@ REmatch best!
       <ThemeProvider theme={darkTheme}>
         <CssBaseline />
         <Container className="container">
+          <Backdrop
+            open={this.state.uploadingFile} 
+            style={{ zIndex: 6000, display: 'flex', flexDirection: 'column' }}
+          >
+            <CircularProgress color="primary" size="3rem"/>
+            <h2 style={{ color: '#fff' }}>Loading ({this.state.fileProgress}%)</h2>
+          </Backdrop>
           <img className="logo" src={Logo} alt="REmatch" />
           <Paper elevation={5} style={{ overflow: 'hidden' }}>
-            <Backdrop open={this.state.uploadingFile} style={{ zIndex: 10000, display: 'flex', flexDirection: 'column' }}>
-              <CircularProgress size='3rem' />
-              <h2 style={{ color: '#fff' }}>Loading ({this.state.fileProgress}%)</h2>
-            </Backdrop>
-            <Grid container spacing={0}>
+
+            <Grid container>
               {/* Expression */}
               <Grid item xs={12}>
                 <SectionTitle title="Expression" />
               </Grid>
-              <Grid item xs={11}>
+
+              <Grid item sm={10} xs={8}>
                 <div id="queryEditor"></div>
               </Grid>
-              <Grid item xs={1}>
-                <Button color="primary" justify="flex-end" startIcon={<PlayArrow />} onClick={this.runWorker} style={{ display: 'flex', width: '100%', background: 'none !important' }}>
+
+              <Grid item sm={2} xs={4}>
+                <Tooltip title="Run query">
+                <Button 
+                  color="primary" 
+                  variant="text"
+                  startIcon={<PlayArrow />} 
+                  onClick={this.runWorker} 
+                  style={{ 
+                    width: '100%',
+                    height: '100%',
+                    }}>
                   Run
                 </Button>
+                </Tooltip>
               </Grid>
+
               {/* EDITOR */}
               <Grid item xs={12}>
                 <SectionTitle title="Text" />
               </Grid>
+
               <Grid item xs={12}>
                 <div id="textEditor">
                   <input accept="*" id="fileInput" type="file" style={{ display: 'none' }} onChange={this.handleFile} />
@@ -392,10 +250,13 @@ REmatch best!
                   </label>
                 </div>
               </Grid>
+
               {/* RESULTS */}
+
               <Grid item xs={12}>
                 <SectionTitle title="Matches" />
               </Grid>
+
               <Grid item xs={12}>
                 <ResultsTable
                   spanList={this.state.spanList}
@@ -406,6 +267,7 @@ REmatch best!
                 />
               </Grid>
             </Grid>
+
           </Paper>
         </Container>
       </ThemeProvider>
